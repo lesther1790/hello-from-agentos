3a8c31d905a0bdaba1a6c03ee5696beaa47e482d
lesther1790 <hola@lesther.me>
2026-04-20 20:14:57 -0600

    feat: add GET /version endpoint

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
+API_VERSION = "1.0.0"
+
 @app.get("/")
 def root():
     return {"hello": "agentos"}
+
+@app.get("/version")
+def version():
+    return {"version": API_VERSION}
